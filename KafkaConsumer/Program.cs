cd87818fb62dfd1e7fa6daad1904df901d3bd051
Pravin Kumar Kesavan <PKesava@eu.trimblecorp.net>
2019-01-25 16:50:23 +0530

    C:\Users\pkesava\source\repos

---
+++ KafkaConsumer/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Text;
+using KafkaNet;
+using KafkaNet.Model;
+using KafkaNet.Protocol;
+
+namespace KafkaConsumer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the topic:");
+                string topic = Console.ReadLine();
+                Uri uri = new Uri("http://localhost:9092");
+                var options = new KafkaOptions(uri);
+                var router = new BrokerRouter(options);
+                var consumer = new Consumer(new ConsumerOptions(topic, router));
+                foreach (var message in consumer.Consume())
+                {
+                    Console.WriteLine(Encoding.UTF8.GetString(message.Value));
+                }
+                Console.ReadLine();
+            
+        }
+    }
+}
